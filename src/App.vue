32a949667223a3f0ef264d5b3e186e92668738e7
Dteboul-Exc <dteboultornezy@excilys.com>
2020-07-24 09:51:56 +0200

    Template for Header + nav list bar

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,54 @@
+  <style>
+  @import '/assets/Home.css';
+  </style>
 <template>
-  <v-app>
+  <v-app :style="{'background': 'rgb(156,156,193);','background': 'linear-gradient(90deg, rgba(156,156,193,1) 8%, rgba(148,213,224,1) 51%, rgba(187,224,232,1) 89%)'}">>
+        <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title> <router-link to="/" tag="button">Home</router-link></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-antenna</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title><router-link to="/about" tag="button">About</router-link></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar
       app
-      color="primary"
+      color="cyan"
       dark
     >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="assets/excilys.png"
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <b>CBD</b>
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
-    <v-main>
-     <router-link to='/'>Home</router-link>
+    <v-main class=" align-center" >
+     <router-link to='/'></router-link>
      <router-view/>
     </v-main>
   </v-app>
@@ -48,12 +58,13 @@
 
 export default {
   name: 'App',
-
+  props: {
+    source: String
+  },
   components: {
   },
-
   data: () => ({
-    //
+    drawer: null
   })
 }
 </script>
