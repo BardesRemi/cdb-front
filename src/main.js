90535f2e24ccc375e03f52a9b795c6f7af61ebf9
Dteboul-Exc <dteboultornezy@excilys.com>
2020-07-31 10:19:43 +0200

    tentative option selector

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import i18n from './i18n'
-
+import LangFlag from 'vue-lang-code-flags'
 Vue.config.productionTip = false
-
+Vue.component('lang-flag', LangFlag)
 new Vue({
   router,
   vuetify,
